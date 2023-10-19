2abaad52f81174b92b0439c30f59b84ca92fd790
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-10-20 00:48:58 +0100

    Update function1.c

--- function1.c
+++ function1.c
@@ -18,9 +18,16 @@ bool isNumber(const char *str) {
     return true;
 }
 
-void push(stack_t **stack, unsigned int line_number){
+void push(stack_t **stack, const char *value){
     stack_t *elem;
-    int n = line_number;
+    int n;
+
+    if (!isNumber(value)) {
+        fprintf(stderr, "L%d: usage: push integer\n", globalline_number);
+        exit(EXIT_FAILURE);
+    }
+
+    n = atoi(value);
     elem = malloc(sizeof(stack_t));
     
     elem->n = n;
@@ -45,44 +52,4 @@ while(h != NULL){
     h = h->next;
 }
 
-}
-void free_stack(stack_t **stack){
-    stack_t *current_node;
-
-    while (*stack != NULL){
-        current_node = (*stack)->next;
-        free(*stack);
-        *stack = current_node;
-    }
-}
-
-/**
- * f_swap - adds the top two elements of the stack.
- * @stack: stack head
- * @line_number: line_number
- * Return: no return
-*/
-void swap(stack_t **stack, unsigned int line_number)
-{
-	stack_t *current_node;
-	int stack_length = 0, temp;
-
-	current_node = *stack;
-	while (current_node)
-	{
-		current_node = current_node->next;
-		stack_length++;
-	}
-	if (stack_length < 2)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		fclose(f.file);
-		free(f.content);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-	current_node = *stack;
-	temp = current_node->n;
-	current_node->n = current_node->next->n;
-	current_node->next->n = temp;
 }
