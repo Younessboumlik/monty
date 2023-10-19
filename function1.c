d972366a84ee0180548c11424c7e3a2a95ce08a6
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-10-19 22:34:17 +0100

    Update function1.c

--- function1.c
+++ function1.c
@@ -1,5 +1,48 @@
 #include "monty.h"
-#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+bool isNumber(const char *str) {
+	    if (str == NULL || *str == '\0') {
+		            return false;
+			        }
+
+	        while (*str != '\0') {
+			        if (*str < '0' || *str > '9') {
+					            return false;
+						            }
+				        str++;
+					    }
+
+		    return true;
+}
+void push(stack_t **stack, unsigned int line_number){
+	stack_t *elem;
+	int n = line_number;
+	elem = malloc(sizeof(stack_t));
+	
+	elem->n = n;
+	elem->next = *stack;
+	elem->prev = NULL;
+	if (*stack != NULL){
+		(*stack)->prev = elem;
+	}
+	*stack = elem;
+	
+}
+void pall(stack_t **stack,unsigned int line_number)
+{
+	     stack_t *h ;
+	(void) line_number;
+	
+	h = *stack;
+	while(h != NULL){
+		printf("%d\n",h->n);
+		
+		h = h->next;
+	}
+	
+}
 /**
  * _nop - do not do nothing.
  *
