9f504069b554c8c6e76180fcc54f66ebb59c658e
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-10-20 00:38:21 +0100

    Update function1.c

--- function1.c
+++ function1.c
@@ -46,3 +46,34 @@ while(h != NULL){
 }
 
 }
+#include "monty.h"
+/**
+ * f_swap - adds the top two elements of the stack.
+ * @stack: stack head
+ * @line_number: line_number
+ * Return: no return
+*/
+void f_swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current_node;
+	int stack_length = 0, temp;
+
+	current_node = *stack;
+	while (current_node)
+	{
+		current_node = current_node->next;
+		stack_length++;
+	}
+	if (stack_length < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	current_node = *stack;
+	temp = current_node->n;
+	current_node->n = current_node->next->n;
+	current_node->next->n = temp;
+}
