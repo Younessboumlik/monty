af2701d8d0af57bc2a2521a71bf988442ad33f98
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-10-19 23:09:41 +0100

    Update function1.c

--- function1.c
+++ function1.c
@@ -43,12 +43,3 @@ void pall(stack_t **stack,unsigned int line_number)
 	}
 	
 }
-/**
- * _nop - do not do nothing.
- *
- */
-void _nop(stack_t **stack, unsigned int line_number)
-{
-	(void) stack;
-	(void) line_number;
-}
