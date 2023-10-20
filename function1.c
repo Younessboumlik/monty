d90f83a60f843789f4c559d2e2ea94151470ecbc
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-10-20 01:10:42 +0100

    Update function1.c

--- function1.c
+++ function1.c
@@ -43,7 +43,7 @@ void pall(stack_t **stack,unsigned int line_number)
 	}
 	
 }
-void f_pint(stack_t **stack, unsigned int line_number)
+void pint(stack_t **stack, unsigned int line_number)
 {
     if (*stack == NULL)
     {
