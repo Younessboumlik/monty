8ca29ecd66614ddd6dca16bf0240cb045252f2b4
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-10-20 01:27:06 +0100

    Update function1.c

--- function1.c
+++ function1.c
@@ -78,3 +78,24 @@ void pint(stack_t **stack, unsigned int line_number)
 
     printf("%d\n", (*stack)->n);
 }
+/**
+ * pop - removes the top element of the stack
+ * @stack: double pointer to top of stack
+ * @line_number: line number
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+    stack_t *temp;
+
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    temp = (*stack)->next;
+    free(*stack);
+    *stack = temp;
+    if (*stack != NULL)
+        (*stack)->prev = NULL;
+}
