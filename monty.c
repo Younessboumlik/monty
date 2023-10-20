3b0e9c2fe94916e8daaffcea77b3a5fd2783c323
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-10-20 01:20:41 +0100

    Update monty.c

--- monty.c
+++ monty.c
@@ -6,69 +6,99 @@
 #include <string.h>
 #include <stdbool.h>
 
-void nop(stack_t **stack, unsigned int line_number) {
+/**
+ * nop - does nothing
+ * @stack: double pointer to the stack to be manipulated
+ * @line_number: number of lines in case of an error
+ *
+ * Description: opcode that doesn’t do anything
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
     (void)stack;
     (void)line_number;
 }
 
-int main(int argc,char **argv){
-        FILE *f;
-        char *token;
-        unsigned int n;
-	int nbr_line=1;
-	char *line = NULL;
-	int i;
-	size_t a=0;
-	stack_t *stack = NULL;
-        instruction_t instr[] = {{"push",&push},{"pall",&pall},{"nop",&nop},{"pint",&pint}};
-        if(argc != 2){
-                fprintf(stderr, "USAGE: monty file\n");
-                exit(EXIT_FAILURE);
-        }
-       f = fopen(argv[1],"r");
-       if (f == NULL){
-                fprintf(stderr, "Error: Can't open file %s\n",argv[1]);
-        	exit(EXIT_FAILURE);
-       }
-       else {
-             while (getline(&line, &a, f) != -1) {
-		    if(line[strlen(line)-1] == '\n'){
-		     line[strlen(line)-1] = '\0';
-		    }
-                    token = strtok(line," ");
-                    while (token != NULL){
-                       for(i =0;i<3;i++){
-                        if (strcmp(token,instr[i].opcode)==0){
-                           break;
-                        }
-                      }
-                      if(i==3){
-                          fprintf(stderr, "L%d: unknown instruction %s\n",nbr_line, token);
-                          exit(EXIT_FAILURE);
-                      }
-                        token = strtok(NULL," ");
-                        if(token != NULL){
-				if (isNumber(token)== false && strcmp(instr[i].opcode,"push")==0){
-                                     fprintf(stderr, "L%d: usage: push integer\n",nbr_line);
-				      exit(EXIT_FAILURE);
-				}
-                         n = atoi(token);
-			break;
-                        }
-                         else{
-		          if(strcmp(instr[i].opcode,"push")== 0){
-                                fprintf(stderr, "L%d: usage: push integer\n",nbr_line);
-                              exit(EXIT_FAILURE);
-			  }
-                            n = 0;
-                         }
+/**
+ * main - entry point for monty bytecode interpreter
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ */
+int main(int argc, char **argv)
+{
+    FILE *f;
+    char *token;
+    unsigned int n;
+    int nbr_line = 1;
+    char *line = NULL;
+    int i;
+    size_t a = 0;
+    stack_t *stack = NULL;
+    instruction_t instr[] = {{"push", &push}, {"pall", &pall}, {"nop", &nop}, {"pint", &pint}};
 
-                   }
-                   (instr[i].f)(&stack,n);
-		   nbr_line++;
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+    f = fopen(argv[1], "r");
+    if (f == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        while (getline(&line, &a, f) != -1)
+        {
+            if (line[strlen(line) - 1] == '\n')
+            {
+                line[strlen(line) - 1] = '\0';
             }
-       }
-       fclose(f);
-       free(line);
-       return 0;
-}        
+            token = strtok(line, " ");
+            while (token != NULL)
+            {
+                for (i = 0; i < 3; i++)
+                {
+                    if (strcmp(token, instr[i].opcode) == 0)
+                    {
+                        break;
+                    }
+                }
+                if (i == 3)
+                {
+                    fprintf(stderr, "L%d: unknown instruction %s\n", nbr_line, token);
+                    exit(EXIT_FAILURE);
+                }
+                token = strtok(NULL, " ");
+                if (token != NULL)
+                {
+                    if (isNumber(token) == false && strcmp(instr[i].opcode, "push") == 0)
+                    {
+                        fprintf(stderr, "L%d: usage: push integer\n", nbr_line);
+                        exit(EXIT_FAILURE);
+                    }
+                    n = atoi(token);
+                    break;
+                }
+                else
+                {
+                    if (strcmp(instr[i].opcode, "push") == 0)
+                    {
+                        fprintf(stderr, "L%d: usage: push integer\n", nbr_line);
+                        exit(EXIT_FAILURE);
+                    }
+                    n = 0;
+                }
+
+            }
+            (instr[i].f)(&stack, n);
+            nbr_line++;
+        }
+    }
+    fclose(f);
+    free(line);
+    return (0);
+}
