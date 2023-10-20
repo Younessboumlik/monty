66c7450b3f72604f369eba60a20768daf594f739
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-10-20 02:10:46 +0100

    Update function1.c

--- function1.c
+++ function1.c
@@ -99,37 +99,3 @@ void pop(stack_t **stack, unsigned int line_number)
     free(*stack);
     *stack = temp;
 }
-}
-/**
- * add - adds the top two elements of the stack.
- * @stack: double pointer to the top of the stack.
- * @line_number: line number.
- *
- * This function adds the top two elements of the stack. 
- * If the stack contains less than two elements, it prints an error message and exits. 
- * After adding, it removes the top element of the stack and replaces it with the result.
- */
-void add(stack_t **stack, unsigned int line_number)
-{
-    stack_t *h = *stack, *head = *stack;
-    int sum = 0;
-    int n = 2;
-    while (head != NULL && n > 0)
-    {
-        sum += head->n;
-        head = head->next;
-        n--;
-    }
-    if (n == 0)
-    {
-        (*stack) = h->next;
-        free(h);
-        (*stack)->n = sum;
-    }
-    else
-    {
-        fprintf(stderr, "L%d: can't add, stack too short", line_number);
-        exit(EXIT_FAILURE);
-    }
-}
-
